Rename to heston_fourier_price, fix SVI total variance

pricing/heston.py
@@ -9,7 +9,7 @@ def heston_char_func(u, S, K, T, r, kappa, theta, xi, rho, v0):
     charFunction = np.exp(1j * u * np.log(forwardPrice) + (kappa * theta / xi**2) * ((kappa - rho * xi * u * 1j - d) * T - 2 * np.log((1 - g * np.exp(-d * T)) / (1 - g))) + (v0 / xi**2) * (kappa - rho * xi * u * 1j - d) * ((1 - np.exp(-d * T)) / (1 - g * np.exp(-d * T))))
     return charFunction
 
-def heston_cos_price(S, K, T, r, kappa, theta, xi, rho, v0, option_type="call", N=100):
+def heston_fourier_price(S, K, T, r, kappa, theta, xi, rho, v0, option_type="call", N=100):
     F = S * np.exp(r * T)
 
     def P(phi_sign):
@@ -38,16 +38,16 @@ def calibrate_heston(strikes, expiries, ivs, S, r):
         heston_prices = []
         kappa, theta, xi, rho, v0 = params
         for strike, expiry in zip(strikes, expiries):
-            heston_prices.append(heston_cos_price(S, strike, expiry, r, kappa, theta, xi, rho, v0, "call"))
+            heston_prices.append(heston_fourier_price(S, strike, expiry, r, kappa, theta, xi, rho, v0, "call"))
         return np.sum(np.square(np.array(heston_prices) - np.array(market_prices)))
 
-    result = optimize.minimize(loss, [2, 0.04, 0.3, -0.7, 0.04], bounds=[(0.001, 20), (0.001, 1), (0.001, 5), (-0.999, 0.999), (0.001, 1)])
+    result = optimize.minimize(loss, [2, 0.04, 0.3, -0.7, 0.04], bounds=[(0.001, None), (0.001, None), (0.001, None), (-0.999, 0.999), (0.001, None)])
     return result.x
 
 def main():
-    testCall1Val = heston_cos_price(100, 100, 1, 0.05, 2, 0.04, 0.3, -0.7, 0.04, "call", 100)
+    testCall1Val = heston_fourier_price(100, 100, 1, 0.05, 2, 0.04, 0.3, -0.7, 0.04, "call", 100)
     print(f"{testCall1Val:.2f} is the call price for test 1.")
-    testPutVal = heston_cos_price(100, 100, 1, 0.05, 2, 0.04, 0.3, -0.7, 0.04, "put", 100)
+    testPutVal = heston_fourier_price(100, 100, 1, 0.05, 2, 0.04, 0.3, -0.7, 0.04, "put", 100)
     print(f"{testPutVal:.2f} is the put price for test 2.")
 
 if __name__ == "__main__":

pricing/svi.py
@@ -7,8 +7,7 @@ def svi_variance(k, a, b, rho, m, sigma):
 
 def fit_svi(strikes, ivs, F, T):
     k_values = np.log(np.array(strikes) / F)
-    impliedVolatilityVals = np.square(np.array(ivs))
-
+    impliedVolatilityVals = np.square(np.array(ivs)) * T
     def lossHelper(params):
         a, b, rho, m, sigma = params
         return np.sum(np.square(svi_variance(k_values, a, b, rho, m, sigma) - impliedVolatilityVals))