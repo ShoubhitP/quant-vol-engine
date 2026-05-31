d45fcc795968e8c9b84b766f6e3903fa833449d3
ShoubhitP <spusulur24@gmail.com>
2026-05-30 19:17:45 -0500

    Add pybind11 C++ Monte Carlo pricing

--- cpp/quant_cpp.cpp
+++ cpp/quant_cpp.cpp
@@ -1,4 +1,7 @@
 #include <cmath>
+#include <random>
+#include <algorithm>
+#include <tuple>
 #include <pybind11/pybind11.h>
 
 namespace py = pybind11;
@@ -31,10 +34,63 @@ double bs_put_cpp(double S, double K, double vol, double r, double T)
     return bs_call_cpp(S, K, vol, r, T) + K * std::exp(-r * T) - S;
 }
 
+double single_stock_price_cpp(
+    double stock_price,
+    double vol,
+    double r,
+    double time_to_expiry,
+    double z)
+{
+    return stock_price * std::exp(
+                             (r - 0.5 * vol * vol) * time_to_expiry + vol * std::sqrt(time_to_expiry) * z);
+}
+
+std::tuple<double, double, double> mc_call_cpp(double stock_price, double strike_price, double vol, double r, double time_to_expiry, int num_simulations)
+{
+    std::random_device rd;
+    std::mt19937 generator(rd());
+    std::normal_distribution<double> standard_normal(0.0, 1.0);
+
+    double sum = 0.0;
+    double sum_sq = 0.0;
+
+    for (int i = 0; i < num_simulations; i++)
+    {
+        double z = standard_normal(generator);
+
+        double positive_price = single_stock_price_cpp(
+            stock_price, vol, r, time_to_expiry, z);
+
+        double negative_price = single_stock_price_cpp(
+            stock_price, vol, r, time_to_expiry, -z);
+
+        double payoff = (std::max(positive_price - strike_price, 0.0) + std::max(negative_price - strike_price, 0.0)) / 2.0;
+
+        sum += payoff;
+        sum_sq += payoff * payoff;
+    }
+
+    double average_payoff = sum / num_simulations;
+    double discount_factor = std::exp(-r * time_to_expiry);
+
+    double sample_variance =
+        (sum_sq - num_simulations * average_payoff * average_payoff) / (num_simulations - 1);
+
+    double standard_error = std::sqrt(sample_variance / num_simulations);
+    double margin = 1.96 * standard_error * discount_factor;
+
+    double price = average_payoff * discount_factor;
+    double lower = price - margin;
+    double upper = price + margin;
+
+    return std::make_tuple(price, lower, upper);
+}
+
 PYBIND11_MODULE(quant_cpp, m)
 {
     m.doc() = "C++ pricing extensions for Quant Vol Engine";
 
     m.def("bs_call", &bs_call_cpp, "Black-Scholes call price");
     m.def("bs_put", &bs_put_cpp, "Black-Scholes put price");
+    m.def("mc_call", &mc_call_cpp, "Monte Carlo call price with antithetic variates");
 }
--- tests/test_quant_cpp.py
+++ tests/test_quant_cpp.py
@@ -12,3 +12,9 @@ def test_cpp_bs_put_matches_python():
     py_price = black_scholes_put(100, 100, 0.2, 0.05, 1)
 
     assert abs(cpp_price - py_price) < 1e-8
+
+def test_cpp_mc_call_contains_black_scholes_price():
+    price, lower, upper = quant_cpp.mc_call(100, 100, 0.2, 0.05, 1, 100000)
+    bs_price = black_scholes_call(100, 100, 0.2, 0.05, 1)
+
+    assert lower <= bs_price <= upper
